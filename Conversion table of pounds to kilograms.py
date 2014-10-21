36d2652ecaae84bf3bee74474a534e09e98db76d
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-10-21 12:58:11 +0100

    conversion table of pounds to kg

---
+++ Conversion table of pounds to kilograms.py
@@ -0,0 +1,7 @@
+#Lewis Travers
+#21/10/2014
+#Conversion table of pounds to kilograms
+for count in range(1,21):
+    kilograms = 1 * count
+    pounds = kilograms * 2.2
+    print("| {0:^2} | {1:^2} |".format(kilograms,pounds))
