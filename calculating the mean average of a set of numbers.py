74a3987d067d18061bcc73d81caf560592296c0f
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-11-13 09:08:01 +0000

    calculating the mean average of a set of numbers

---
+++ calculating the mean average of a set of numbers.py
@@ -0,0 +1,10 @@
+#Lewis Travers
+#31/10/2014
+#calculating the mean average of a set of numbers
+
+amount = int(input("Please enter how many numbers are to be averaged: "))
+total = 0
+for count in range(amount):
+    number = int(input("Please enter a number"))
+    number + total = total
+    print(total)
